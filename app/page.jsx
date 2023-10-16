530093dd6c5f89483fe1541179508f10a432e397
jimjamesjimathy <jamesalanmikal@gmail.com>
2023-10-16 11:31:10 -0400

    basic boiler plate for quick start up

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <section className="w-full min-h-screen flex items-center justify-center">
+        <h1>header</h1>
+      </section>
+    </main>
+  );
+}
